85f8a4564f6ccf37c72ca0165c7a652ab0c54831
siyam <siyam@pop-os.localdomain>
2024-06-01 00:26:53 +0530

    adds operate function

--- index.js
+++ index.js
@@ -14,5 +14,19 @@ divide = function(a,b){
     return a/b;
 }
 
+let num1,num2,operator;
 
-console.log(multiply(2,3));
+operate = function(operator,num1,num2){
+    if(operator==add){
+       return add(num1,num2);
+    }
+    else if(operator==subtract){
+        return subtract(num1,num2);
+    }
+    else if(operator==multiply){
+        return multiply(num1,num2);
+    }
+    else if(operator==divide){
+        return divide(num1,num2);
+    }
+}
