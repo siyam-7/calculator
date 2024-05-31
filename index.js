de4a6b4d7a200111a369ca51d4f09c8e103852ca
siyam <siyam@pop-os.localdomain>
2024-06-01 00:07:31 +0530

    Create add, subtract, multiply and divide functions

---
+++ index.js
@@ -0,0 +1,18 @@
+add = function(a,b){
+    return a+b;
+}
+
+subtract = function(a,b){
+    return a-b;
+}
+
+multiply = function(a,b){
+    return a*b;
+}
+
+divide = function(a,b){
+    return a/b;
+}
+
+
+console.log(multiply(2,3));
